444b1ec36490334ca329365414bb1ef3b49528f3
ldg26 <ldg26@njit.edu>
2025-05-06 15:53:52 -0400

    Update urls.py

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("index/", views.index, name="index"), 
     path("about/", views.about, name="about"),  
-    path("contact/", views.contact, name="contact")
-]
+    path("contact/", views.contact, name="contact"),
+    
+    # Feedback system
+    path("feedback/", views.feedback_view, name="feedback"),
