091661839c0612a19d59cf27a8c6bb550f1d2c3a
ldg26 <ldg26@njit.edu>
2025-05-06 15:39:59 -0400

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import ReviewForm, RegistrationForm, LaptopForm
 
 # Create your views here.
 
@@ -15,6 +16,17 @@ def about(request):
 def contact(request): 
     context = {"Email": "example@gmail.com"} 
     return render(request, "homepage/contact.html", context)
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('thank_you')  # after submitting feedback
+    else:
+        form = ReviewForm()
+    return render(request, 'homepage/feedback.html', {'form': form})
+
 
 
 
